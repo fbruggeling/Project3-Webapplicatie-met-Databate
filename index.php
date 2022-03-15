416b5d4ac4fac2268d6025ac4d07499dd79a01df
fbruggeling <fbruggeling01@gmail.com>
2022-03-15 19:29:37 +0100

    2nd commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="utf-8">
+    <title>Spaceinvaders</title>
+  </head>
+  <body>
+    <h1>Login to go further</h1><br>
+
+    <p>We are sorry but if you want to play the game you need to sign in or create a new account</p><br><br>
+    <p><a href="login.php">Login</p><br><br>
+    <p><a href="register.php">Register a new account</p><br>
+  </body>
+</html>
